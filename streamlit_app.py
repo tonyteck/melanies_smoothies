b776b622081f1267a1ab45dd49d5ed14f33fa181
tonyteck <tjpraseu@alumni.ucalgary.ca>
2026-04-02 17:34:17 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 # Import python packages
 import requests
-smoothiefroot_response = requests.get('https://my.smoothiefroot.com/api/fruit/watermelon")
-                                      st.text(smoothiefroot_response.json())
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+                                      # st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 import streamlit as st
 from snowflake.snowpark.functions import col
 
